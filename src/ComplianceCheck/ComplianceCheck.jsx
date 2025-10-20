8cabed372cadcde31f241aa48eb079ef71651bca
S.M <139430741+hades255@users.noreply.github.com>
2025-10-21 08:59:43 +0900

    add toast

--- src/ComplianceCheck/ComplianceCheck.jsx
+++ src/ComplianceCheck/ComplianceCheck.jsx
@@ -1,11 +1,16 @@
 import ChecksPage from "./ChecksPage";
+import ToastContainer from "./components/toast";
 import { ChecksProvider } from "./contexts/ChecksContext";
+import { ToastProvider } from "./contexts/ToastContext";
 
 function ComplianceCheck() {
   return (
-    <ChecksProvider>
-      <ChecksPage />
-    </ChecksProvider>
+    <ToastProvider>
+      <ChecksProvider>
+        <ChecksPage />
+      </ChecksProvider>
+      <ToastContainer />
+    </ToastProvider>
   );
 }
 
